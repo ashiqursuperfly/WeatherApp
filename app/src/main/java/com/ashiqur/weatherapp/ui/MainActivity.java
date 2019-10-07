9524187067e05d1d081ebff4346afc5d97275ea0
ashiqursuperfly <ashiqur.superfly@gmail.com>
2019-10-07 16:56:26 +0600

    Refactoring:hammer:

--- app/src/main/java/com/ashiqur/weatherapp/ui/MainActivity.java
+++ app/src/main/java/com/ashiqur/weatherapp/ui/MainActivity.java
@@ -1,10 +1,10 @@
-package com.ashiqur.weatherapp;
+package com.ashiqur.weatherapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.ashiqur.weatherapp.ui.MainActivityViewModel;
+import com.ashiqur.weatherapp.R;
 import com.ashiqur.weatherapp.utils.ViewModelUtils;
 
 public class MainActivity extends AppCompatActivity {
